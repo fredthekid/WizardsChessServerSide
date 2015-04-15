b973c16c060f658c21058d4890dd0336ec8390d9
Fred <fredthekid@gmail.com>
2015-04-15 08:24:08 -0700

    Displays chessboard

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     var messages = [];
-    var socket = io.connect('http://192.168.1.103:8080');
+    var socket = io.connect('http://localhost:8080');
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
--- public/js/chessapp.js
+++ public/js/chessapp.js
@@ -1,9 +1,15 @@
-var cfg = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd
-};
+window.onload = function(){
+    var socket = io.connect('http://localhost:8080')
+    
+    /*
+    var cfg = {
+        draggable: true,
+        position: 'start',
+        onDragStart: onDragStart,
+        onDrop: onDrop,
+        onSnapEnd: onSnapEnd
+    };
+    */
 
-var board = new ChessBoard('board', cfg);
+    var board = new ChessBoard('board');
+}
