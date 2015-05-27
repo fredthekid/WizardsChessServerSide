4b1b39cf921b8e6c2dfe81de162ed2eda8bcac83
fredthekid <fredthekid@gmail.com>
2015-05-26 17:44:13 -0700

    Update chessclient.js

--- public/js/chessclient.js
+++ public/js/chessclient.js
@@ -7,21 +7,6 @@ window.onload = function(){
     var clientfen = 'start';
     var lastMove;
 	
-
-    // do not pick up pieces if the game is over
-    // only pick up pieces for White
-
-	
-	//Old DragStart
-	/*
-    var onDragStart = function(source, piece, position, orientation) {
-        if (game.in_checkmate() === true || game.in_draw() === true ||
-                piece.search(/^b/) !== -1) {
-            return false;
-        }
-    };
-	*/
-	
 	//New DragStart
 	var onDragStart = function(source, piece, position, orientation) {
 	  if (game.game_over() === true ||
@@ -30,23 +15,6 @@ window.onload = function(){
 		return false;
 	  }
 	};
-
-	/*
-	//Old onDrop
-    var onDrop = function(source, target, piece, newPos, oldPos, orientation) {
-        // see if the move is legal
-        var move = game.move({
-            from: source,
-            to: target,
-            promotion: 'q' // NOTE: always promote to a queen for example simplicity
-        });
-
-        // illegal move
-        if (move === null) return 'snapback';
-        // make random legal move for black
-        window.setTimeout(makeRandomMove, 250);
-    };
-	*/
 	
 	var onDrop = function(source, target) {
 	  // see if the move is legal
@@ -63,26 +31,8 @@ window.onload = function(){
 	  //updateStatus();
 	};
 
-	/*
-	//this is used with the older code
-    var makeRandomMove = function() {
-        var possibleMoves = game.moves();
-        // game over
-        if (possibleMoves.length === 0) return;
-
-        var randomIndex = Math.floor(Math.random() * possibleMoves.length);
-        game.move(possibleMoves[randomIndex]);
-		updateServerGame();
-    };
-	*/
-
-    // update the board position after the piece snap
-    // for castling, en passant, pawn promotion
     var onSnapEnd = function() {
         updateServerGame();
-		
-		//may need to use this function later, take it out for now
-		//board.position(game.fen());
     };
 	
 	
